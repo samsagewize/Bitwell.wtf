e722cbc45f91956170627811740226b5136e15d3
Thaddeus Diamond <thaddeus.diamond@gmail.com>
2023-08-13 23:36:24 -0500

    Picker Pagination: Enable a quick pagination button
    The DOM was failing to load simply due to its size.  The issue here is
    not image optimization but the HTML size alone, which was causing
    `view-source` even to not load in localhost.  This change adds a simple
    pagination feature so that you can actually load and move through the
    different inscription options.
    
    [ Testing: npm run build, npm run dev ]

--- mint/src/components/picker.jsx
+++ mint/src/components/picker.jsx
@@ -2,10 +2,14 @@
 
 import Image from 'next/image';
 
+import { useState } from 'react';
+
 import { INSCRIPTION_CDN } from '../config/ordinals.js';
 
 export const IMAGE_TYPE = 'image';
 export const IFRAME_TYPE = 'iframe';
+
+const INSCRIPTIONS_PER_PAGE = 500;
 
 function componentFor(type, label, src) {
   if (type === IMAGE_TYPE) {
@@ -20,8 +24,14 @@ function componentFor(type, label, src) {
 }
 
 export function InscriptionPicker({ type, inscriptions, selectedAttribute, setSelectedAttribute }) {
+  const [page, setPage] = useState(1);
+
+  const inscriptionNames = Object.keys(inscriptions);
+  const maxPages = Math.ceil(inscriptionNames.length / INSCRIPTIONS_PER_PAGE);
+  const currentMin = (page - 1) * INSCRIPTIONS_PER_PAGE;
+  const currentMax = page * INSCRIPTIONS_PER_PAGE;
   const inscriptionsDoms = [];
-  for (const key of Object.keys(inscriptions)) {
+  for (const key of inscriptionNames.slice(currentMin, currentMax)) {
     const isSelected = (selectedAttribute === key);
     inscriptionsDoms.push(
       <div key={key} className={`p-2 cursor-pointer border-2 ${isSelected ? 'border-bitwell-blue' : 'border-transparent'}`} onClick={() => setSelectedAttribute(key)}>
@@ -34,8 +44,23 @@ export function InscriptionPicker({ type, inscriptions, selectedAttribute, setSe
   }
 
   return (
-    <div className="flex flex-wrap justify-center items-start text-center text-sm max-h-96 overflow-hidden overflow-scroll w-full gap-4">
-      {inscriptionsDoms}
+    <div>
+      <div className="flex flex-wrap justify-center items-start text-center text-sm max-h-96 overflow-hidden overflow-scroll w-full gap-4">
+        {inscriptionsDoms}
+      </div>
+      {maxPages > 1 ? (
+        <div className="flex justify-center gap-8 mt-2">
+          <div className={`${page > 1 ? 'text-black' : 'text-gray-400'} select-none cursor-pointer`} onClick={() => setPage(Math.max(1, page - 1))}>
+            &lt;
+          </div>
+          <div>
+            {page}
+          </div>
+          <div className={`${page < maxPages ? 'text-black' : 'text-gray-400'} select-none cursor-pointer`} onClick={() => setPage(Math.min(maxPages, page + 1))}>
+            &gt;
+          </div>
+        </div>
+      ) : undefined}
     </div>
   );
 }
