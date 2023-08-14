a276c3c104ae6d86d28296a4a32057a3b6de8f85
Thaddeus Diamond <thaddeus.diamond@gmail.com>
2023-08-13 23:40:57 -0500

    Minor UX fix for cursor pointer and selection

--- mint/src/components/section.jsx
+++ mint/src/components/section.jsx
@@ -5,9 +5,9 @@ import { useState } from 'react';
 export function Section({ isExpanded, onClick, label, children }) {
   return (
     <div className="text-black text-lg bg-white border-t-2 border-t-bitwell-blue">
-      <div className="p-2 flex justify-between gap-8 border-b-2 border-b-bitwell-blue" onClick={onClick}>
+      <div className="p-2 flex justify-between gap-8 border-b-2 border-b-bitwell-blue select-none cursor-pointer" onClick={onClick}>
         {label}
-        <div className="font-bold cursor-pointer">
+        <div className="font-bold">
           {isExpanded ? '▼' : '◀'}
         </div>
       </div>
