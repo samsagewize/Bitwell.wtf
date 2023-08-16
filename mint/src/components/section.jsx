30c497f1a04444c861013462636367bb045f8351
Thaddeus Diamond <thaddeus.diamond@gmail.com>
2023-08-16 11:54:15 -0500

    Fix left icon arrow (which renders weirdly on iOS)

--- mint/src/components/section.jsx
+++ mint/src/components/section.jsx
@@ -1,5 +1,7 @@
 'use client';
 
+import Image from 'next/image';
+
 import { useState } from 'react';
 
 export function Section({ isExpanded, onClick, label, children }) {
@@ -7,8 +9,8 @@ export function Section({ isExpanded, onClick, label, children }) {
     <div className="text-white text-lg bg-bitwell-purple border-t-2 border-t-white">
       <div className="p-2 flex justify-between gap-8 border-b-2 border-b-white select-none cursor-pointer" onClick={onClick}>
         {label}
-        <div className="font-bold">
-          {isExpanded ? '▼' : '◀'}
+        <div className="font-bold flex items-center justify-center p-1">
+          <Image src={`/${isExpanded ? 'down_arrow' : 'left_arrow'}.png`} width={18} height={18} alt={isExpanded ? 'collapse' : 'expand'} style={{imageRendering: 'pixelated'}} />
         </div>
       </div>
       <div className={`flex p-4 gap-4 ${isExpanded ? '' : 'hidden'}`}>
