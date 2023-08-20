e23dec46f008e3aa63535a0ecc0e0baff2d1a870
Thaddeus Diamond <thaddeus.diamond@gmail.com>
2023-08-20 11:05:40 -0500

    feat(/api/cron/find_inscriptions): Add crontab for paid but not inscribed
    We introduced a breaking change where we consider an item "minted" so
    long as the item has been "paid" on the invoice.  However, very often
    this will lead to inscriptions not being present because the invoice has
    been paid but OrdinalsBot is still clearing the queue.
    
    This change adds another GET endpoint (which we put on a 20-minute
    Vercel crontab) at `/api/cron/find_inscriptions` that does the exact
    same thing as `/api/cron/update` except only searches where inscription
    is empty but the item was paid for.
    
    [ Testing: npm run build, npm run dev, manual QA ]

---
+++ mint/src/app/api/cron/find_inscriptions/route.js
@@ -0,0 +1,58 @@
+import { NextResponse } from 'next/server';
+
+import prisma from '../../../../prisma/prisma.mjs';
+
+import { DEFAULT_ORDER_API } from '../../../../config/ordinalsbot.js';
+
+export const revalidate = 0;
+
+const sleep = (ms = 0) => new Promise(resolve => setTimeout(resolve, ms));
+
+const HALF_SEC_MS = 500;
+const BACKOFF_MS = 5 * HALF_SEC_MS;
+
+export async function GET() {
+  try {
+    const paidOrders = await prisma.bitwell.findMany({
+      where: {
+        minted: true,
+        inscription: ''
+      }
+    });
+
+    var updatedOrderNum = 0;
+    for (const paidOrder of paidOrders) {
+      await sleep(HALF_SEC_MS);
+      const paidOrderStatusReq = await fetch(`${DEFAULT_ORDER_API}?id=${paidOrder.order_id}`);
+      if (paidOrderStatusReq.status !== 200) {
+        console.error(`Could not retrieve order status for order "${paidOrder.id}" (${paidOrderStatusReq.status}): ${paidOrderStatusReq.statusText}`);
+        await sleep(BACKOFF_MS);
+        continue;
+      }
+      const paidOrderStatus = await paidOrderStatusReq.json();
+      const inscription = paidOrderStatus.files[0].tx?.inscription;
+      if (!inscription) {
+        continue;
+      }
+      const updatedOrders = await prisma.bitwell.update({
+        where: { id: paidOrder.id },
+        data: {
+          inscription: inscription,
+          updated_at: new Date()
+       }
+      });
+      console.log(JSON.stringify(updatedOrders));
+      if (updatedOrders.length !== 1) {
+        console.error(`Could not update order status for order #${paidOrder.id}`);
+      }
+      updatedOrderNum++;
+      console.log(`Found paid order #${paidOrder.id} has inscription "${inscription}"`);
+    }
+
+    console.log(`Successfully updated ${updatedOrderNum} orders`);
+    return NextResponse.json({updatedOrders: updatedOrderNum}, {status: 200, statusText: `Successfully updated ${updatedOrderNum} orders`});
+  } catch (err) {
+    console.error(JSON.stringify(err));
+    return NextResponse.json(err, {status: 500, statusText: err});
+  }
+}
