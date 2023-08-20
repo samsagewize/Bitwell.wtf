523cf024c7c28720784b9dcd897a7bef9dd34a72
Thaddeus Diamond <thaddeus.diamond@gmail.com>
2023-08-20 10:41:12 -0500

    fix(Latest Mints): Account for empty (processing) inscriptions

--- mint/src/components/log.jsx
+++ mint/src/components/log.jsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import { INSCRIPTION_CDN } from '../config/ordinals.js';
 
 export function LatestMints({ mintedPunks }) {
@@ -7,16 +9,19 @@ export function LatestMints({ mintedPunks }) {
     latestMints.push(
       <div key={mintedPunk.inscription} className={`mx-auto flex truncate justify-center w-fit max-w-full text-black items-center align-center py-4 gap-8 border-t-2 ${isFirst ? 'border-black' : 'border-gray-300'}`}>
         <div className="w-fit max-w-[5rem]">
-          <iframe className="border-none w-full aspect-square" src={`${INSCRIPTION_CDN}/${mintedPunk.inscription}`} />
+          {mintedPunk.inscription ?
+            <iframe className="border-none w-full aspect-square" src={`${INSCRIPTION_CDN}/${mintedPunk.inscription}`} /> :
+            <Image src="/icon.png" width="100" height="100" alt="Inscription processing..." /> }
         </div>
         <div className="w-fit text-xl font-bold" style={{textWrap: 'wrap'}}>
           Bitwell {mintedPunk.name}
         </div>
         <div className="w-min text-start truncate">
           <div className="truncate text-sm md:text-lg">
-            <a href={`https://ord.io/${mintedPunk.inscription}`} target="_blank">
-              {mintedPunk.inscription}
-            </a>
+            {mintedPunk.inscription ?
+              <a href={`https://ord.io/${mintedPunk.inscription}`} target="_blank">{mintedPunk.inscription}</a> :
+             'Processing, please wait up to 24 hours for inscription to arrive at the address below'
+            }
           </div>
           <div className="truncate text-gray-400 text-sm italic">
             <a href={`https://ord.io/${mintedPunk.ordinals_addr}`} target="_blank">
