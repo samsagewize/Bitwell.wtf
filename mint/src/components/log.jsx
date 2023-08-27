a74a7979f93edae87c881d66c34fc91576028ed0
Thaddeus Diamond <thaddeus.diamond@gmail.com>
2023-08-27 11:41:36 -0500

    ux(Latest Mints): Change UX to look more like ME

--- mint/src/components/log.jsx
+++ mint/src/components/log.jsx
@@ -7,29 +7,29 @@ export function LatestMints({ mintedPunks }) {
   let isFirst = true;
   for (const mintedPunk of mintedPunks) {
     latestMints.push(
-      <div key={mintedPunk.inscription} className={`mx-auto flex truncate justify-center w-fit max-w-full text-black items-center align-center py-4 gap-8 border-t-2 ${isFirst ? 'border-black' : 'border-gray-300'}`}>
-        <div className="w-fit max-w-[5rem]">
+      <div key={mintedPunk.inscription} className={`max-w-[8rem] md:max-w-[14rem] truncate justify-center text-black items-center align-center py-4`}>
+        <div className="w-full max-w-[8rem] md:max-w-[14rem]">
           {mintedPunk.inscription ?
             <iframe className="border-none w-full aspect-square" src={`${INSCRIPTION_CDN}/${mintedPunk.inscription}`} /> :
-            <Image src="/icon.png" width="100" height="100" alt="Inscription processing..." /> }
+            <Image src="/icon.png" width="200" height="200" alt="Inscription processing..." /> }
         </div>
-        <div className="w-fit text-xl font-bold" style={{textWrap: 'wrap'}}>
+        <div className="text-lg text-start font-bold mt-1" style={{textWrap: 'wrap'}}>
           Bitwell {mintedPunk.name}
         </div>
-        <div className="w-min text-start truncate">
-          <div className="truncate text-sm md:text-lg">
+        <div className="text-start truncate">
+          <div className="truncate text-sm md:text-md">
             {mintedPunk.inscription ?
               <a href={`https://ord.io/${mintedPunk.inscription}`} target="_blank">{mintedPunk.inscription}</a> :
              'Processing, please wait up to 24 hours for inscription to arrive at the address below'
             }
           </div>
-          <div className="truncate text-gray-400 text-sm italic">
+          <div className="truncate text-gray-400 text-sm italic mt-1">
             <a href={`https://ord.io/${mintedPunk.ordinals_addr}`} target="_blank">
               {mintedPunk.ordinals_addr}
             </a>
           </div>
         </div>
-        <div className="w-min text-sm" style={{textWrap: 'wrap'}}>
+        <div className="text-start text-gray-400 text-sm" style={{textWrap: 'wrap'}}>
           {mintedPunk.updated_at.toLocaleString()}
         </div>
       </div>
@@ -37,11 +37,13 @@ export function LatestMints({ mintedPunks }) {
     isFirst = false;
   }
   return (
-    <div className="text-center w-full">
-      <div className="font-main font-wrap text-center text-3xl text-black py-2">
+    <div className="text-center w-fit">
+      <div className="font-main font-wrap text-center text-3xl text-black py-2 border-b-2 border-black">
         Latest Mints
       </div>
-      {latestMints}
+      <div className="flex flex-wrap gap-4 md:gap-8 justify-center items-start">
+        {latestMints}
+      </div>
     </div>
   )
 }
